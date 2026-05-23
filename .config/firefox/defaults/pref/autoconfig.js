f1ea36e135a1040c2c19e6b8058157a5df2004bc
snes <snes.bhandari@mail.utoronto.ca>
2026-05-23 00:22:08 -0400

    fix firefox autoconf

--- .config/firefox/defaults/pref/autoconfig.js
+++ .config/firefox/defaults/pref/autoconfig.js
@@ -1,4 +1,3 @@
-pref("general.config.obscure_value", 0);
 pref("general.config.filename", "mozilla.cfg");
-// Needed for running startup script code
+pref("general.config.obscure_value", 0);
 pref("general.config.sandbox_enabled", false);
